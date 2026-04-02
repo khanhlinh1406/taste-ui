87e87a43afa992ff17a8bc291cba0833d4bf7805
Linh Nguyen <72326769+khanhlinh1406@users.noreply.github.com>
2026-04-02 22:45:24 +0700

    feat(frontend): setup next.js with Tailwind and basic structure

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">TasteUI 🎨</h1>
+    </div>
+  );
+}
---
+++ frontend/lib/api.ts
@@ -0,0 +1,14 @@
+const  API_BASE = 'http://localhost:8000';
+
+export async function extractColors(file: File) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch(`${API_BASE}/extract-colors`, {
+        method: "POST",
+        body: formData
+    });
+
+    return res.json();
+}
+
